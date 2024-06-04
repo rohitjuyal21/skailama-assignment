800722e51d876d7ba308ab81a420ded8bc7368f7
Rohit Juyal <rohitjuyal2003@gmail.com>
2024-06-04 10:18:30 +0530

    update

--- src/components/Contexts/ProjectContext.jsx
+++ src/components/Contexts/ProjectContext.jsx
@@ -1,19 +1,10 @@
 import { createContext, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 export const ProjectContext = createContext();
 
 const ProjectProvider = ({ children }) => {
   const [projects, setProjects] = useState([]);
-  const [projectFiles, setProjectFiles] = useState([
-    {
-      id: uuidv4().slice(0, 8),
-      name: "hey",
-      description: "Hello",
-      status: "Done",
-      createdAt: new Date(),
-    },
-  ]);
+  const [projectFiles, setProjectFiles] = useState([]);
   return (
     <ProjectContext.Provider
       value={{ projects, setProjects, projectFiles, setProjectFiles }}
